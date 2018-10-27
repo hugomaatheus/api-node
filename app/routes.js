04234f4966c3396ca1f97985a804a29ce5f7d88e
hugomatheuss <hugomatheus650@gmail.com>
2018-10-27 12:52:13 -0300

    middleware

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,8 @@ const requireDir = require('require-dir');
 
 const routes = express.Router();
 
+const authMiddleware = require('./middlewares/auth');
+
 const controllers = requireDir('./controllers');
 
 console.log(controllers);
@@ -12,5 +14,16 @@ console.log(controllers);
  */
 routes.post('/signin', controllers.authController.signin);
 routes.post('/signup', controllers.authController.signup);
+
+/**
+ * Auth middleware
+ */
+routes.use(authMiddleware);
+
+routes.get('/tweets', (req, res) => {
+  console.log(req.userId);
+
+  res.send('OK');
+});
 
 module.exports = routes;
