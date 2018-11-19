89638b677aeaa8f847c0685f65db4266a58bc4b2
hugomatheuss <hugomatheus650@gmail.com>
2018-11-19 17:45:18 -0300

    sinon and spy

--- __tests__/integration/auth.test.js
+++ __tests__/integration/auth.test.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 // eslint-disable-next-line import/no-unresolved
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const nodemailer = require('nodemailer');
+
+const transport = {
+  sendMail: sinon.spy(),
+};
+
+sinon.stub(nodemailer, 'createTransport').returns(transport);
 
 const { expect } = chai;
 
@@ -18,6 +26,37 @@ describe('Authentication', () => {
     await User.deleteMany();
   });
 
+  describe('Sign up', () => {
+    it('it should be able to sign up, dogg', async () => {
+      const user = await factory.attrs('User');
+
+      const response = await chai.request(app)
+        .post('/api/signup')
+        .send(user);
+
+      expect(response.body).to.have.property('user');
+      expect(response.body).to.have.property('token');
+    });
+
+    it('it should not be able to sign up with duplicates, remember that yo punk!', async () => {
+      const user = await factory.create('User');
+
+      const user2 = await factory.attrs('User', {
+        email: user.email,
+      });
+
+      console.log(user);
+      console.log(user2);
+
+      const response = await chai.request(app)
+        .post('/api/signup')
+        .send(user2);
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.have.status('error');
+    });
+  });
+
   describe('Sign in', () => {
     it('it should be fucking able to authenticate with valid credentials, do you feel me?', async () => {
       const user = await factory.create('User', {
@@ -30,6 +69,7 @@ describe('Authentication', () => {
 
       expect(response.body).to.have.property('user');
       expect(response.body).to.have.property('token');
+      expect(transport.sendMail.calledOnce).to.be.true;
     });
 
     it('it sould not be able to signin with nonexistent user, ya got it??', async () => {
@@ -43,7 +83,7 @@ describe('Authentication', () => {
 
     it('it should be fucking able to authenticate with valid credentials, do you feel me?', async () => {
       const user = await factory.create('User', {
-        password: 'batman123'
+        password: 'batman123',
       });
 
       const response = await chai.request(app)
